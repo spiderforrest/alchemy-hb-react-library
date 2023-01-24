2ab5e99f92d9a580f36822fdaaca416e890bcbcf
spiderforrest <spider@spood.org>
2023-01-24 14:29:56 -0800

    router set up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import BookList from './components/book/BookList';
+import BookDetail from './components/book/BookDetail';
 import './App.css';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 function App() {
-  // TODO: Add routes to books & views
   return (
-    <main className="container">
-      <h1>Library Catalog</h1>
-      <BookList />
-    </main>
+    <BrowserRouter>
+      <main className="container">
+        <h1>Library Catalog</h1>
+        <Switch>
+          <Route exact path="/catalog">
+            <BookList />
+          </Route>
+          <Route path="/books/:id">
+            <BookDetail />
+          </Route>
+          <Route path="*">
+            <Redirect to="/catalog" />
+          </Route>
+        </Switch>
+      </main>
+    </BrowserRouter>
   );
 }
 
